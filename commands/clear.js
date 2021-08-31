08ec043f8dfa2c774da35508b1d814180186fc3c
buildoman <reedduty@gmail.com>
2021-08-30 23:56:18 -0400

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ module.exports = {
     async execute(message, args, Discord) {
         await message.guild.members.fetch();
         message.guild.channels.cache.forEach((channel)=>{
-            if (channel.name.indexOf('private') != -1) {
+            if (channel.name.indexOf('private') != -1 && channel.topic != undefined) {
                 if (!message.guild.members.cache.has(channel.topic.slice(0, channel.topic.indexOf(" ")))) {
                     channel.delete();
                 }
