c7e7eff31e274d98d47cdfea2b78f8512679a17b
reebcode <reedduty@gmail.com>
2022-05-02 17:54:46 -0400

    overhaul, no longer async

---
+++ src/commands/monthly.ts
@@ -0,0 +1,10 @@
+import { Client, Message, TextChannel } from 'discord.js'
+
+// name: 'clear',
+// description: "Use this command to clear channels of users who left the server.",
+
+export async function monthly(message: Message, args: string[], client: Client) {
+    if (message.member?.roles && message.member?.roles.highest.id == '786628196002037770') {
+        
+    }
+}
